a47ee8154ffb033eec29e388a915d6739936aa27
bracesproul <braceasproul@gmail.com>
2025-04-11 11:14:48 -0700

    fix: Add unique nodes for relfection types

--- src/reflection/index.ts
+++ src/reflection/index.ts
@@ -1,10 +1,36 @@
-import { START, StateGraph } from "@langchain/langgraph";
-import { ReflectionZodState } from "./types.js";
-import { reflect } from "./nodes/reflect.js";
+import { Command, END, START, StateGraph } from "@langchain/langgraph";
+import { ReflectionState, ReflectionZodState } from "./types.js";
+import { fullReflection } from "./nodes/full-reflection.js";
+import { explanationReflection } from "./nodes/explanation-reflection.js";
+
+/**
+ * Routes the reflection based on the change type. If the changeType is
+ * 'explanationChanged', it will route to the explanation reflection node.
+ * Otherwise, it will route to the full reflection node.
+ * 
+ * @param state The current state of the reflection.
+ * @returns The command to route to the appropriate reflection node.
+ */
+function routeReflection(state: ReflectionState): Command {
+  if (state.changeType === "explanationChanged") {
+    return new Command({
+      goto: "explanation_reflection",
+    });
+  }
+  return new Command({
+    goto: "full_reflection",
+  });
+}
 
 const workflow = new StateGraph(ReflectionZodState)
-  .addNode("reflect", reflect)
-  .addEdge(START, "reflect");
+  .addNode("routeReflection", routeReflection, {
+    ends: ["full_reflection", "explanation_reflection"],
+  })
+  .addNode("full_reflection", fullReflection)
+  .addNode("explanation_reflection", explanationReflection)
+  .addEdge(START, "routeReflection")
+  .addEdge("explanation_reflection", END)
+  .addEdge("full_reflection", END);
 
 // TODO: Remove as any once type error fixed
 export const graph = workflow.compile() as any;
---
+++ src/reflection/nodes/explanation-reflection.ts
@@ -0,0 +1,7 @@
+import { ReflectionState, ReflectionUpdate } from "../types.js";
+
+export async function explanationReflection(
+  state: ReflectionState,
+): Promise<ReflectionUpdate> {
+  throw new Error("Not implemented" + state);
+}
